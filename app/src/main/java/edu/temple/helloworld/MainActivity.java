abcb77f5a5b2edb20758afd02265cf4aa5dc72a5
plnguyen12 <tuf63125@temple.edu>
2020-09-10 11:12:26 -0400

    Button function is enabled and displays "You have clicked the button" when clicked.

--- app/src/main/java/edu/temple/helloworld/MainActivity.java
+++ app/src/main/java/edu/temple/helloworld/MainActivity.java
@@ -3,6 +3,9 @@ package edu.temple.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        changeText();
+    }
+
+    // make button click change TextView
+    private void changeText() {
+        final TextView changingText = (TextView) findViewById(R.id.textView);
+        Button TextButton = (Button) findViewById(R.id.button);
+        TextButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                changingText.setText(R.string.message);
+            }
+        });
     }
 }
